f8024c0ff19994ec4bffa39c81858cf431240b5e
AlexYakovl <alalextom@gmail.com>
2025-03-04 20:29:08 +0500

    Migrations task

---
+++ database/migrations/2025_03_04_121327_create_expense_transactions_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('expense_transactions', function (Blueprint $table) {
+            $table->id(); // PK
+            $table->foreignId('account_id')->constrained('accounts')->onDelete('cascade'); // FK
+            $table->decimal('amount', 15, 2); // Сумма
+            $table->timestamp('transaction_time')->useCurrent(); // Дата и время операции
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('expense_transactions');
+    }
+};
