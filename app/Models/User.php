08bf5d184a09101de2635ebe30c9cf5160b4ab5c
AlexYakovl <alalextom@gmail.com>
2025-09-23 17:01:06 +0500

    Установка Sanctum

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,12 +1,13 @@
 <?php
 
 namespace App\Models;
-
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 
 class User extends Authenticatable
 {
+    use HasApiTokens;
     protected $fillable = ['name', 'email', 'password'];
 
     protected $hidden = ['password'];
--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,23 @@ use App\Http\Controllers\Api\ExpenseTransactionApiController;
 use App\Http\Controllers\Api\IncomeTransactionApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/accounts_close', [AccountApiController::class, 'index']);
+    Route::get('/me', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
 
 Route::apiResource('accounts', AccountApiController::class);
